5faa6e9cf60ad6f2cbde49b611d609ccbaff85ab
Maharshi Mishra <ihsraham27@gmail.com>
2021-12-01 11:45:59 +0530

    Changes made

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,15 +14,23 @@ import {
   Div2,
   Div3,
   NavLink,
-  SocialIcons
+  SocialIcons,
+  Span
 } from "./HeaderStyles";
 
 const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20px"
+          }}
+        >
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello There! <br />I am Maharshi
+      </SectionTitle>
+      <SectionText>
+        Mechanical Engineering graduate. Interested in Data Analytics.
+      </SectionText>
+      <Button onClick={() => (window.location = "#")}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
